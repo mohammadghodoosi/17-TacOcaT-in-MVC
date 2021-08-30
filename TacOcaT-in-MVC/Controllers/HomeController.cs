cdce9cd1ad2ec2df0d65802a98822ec438089b48
Mohammad Ghodoosi <ghodusimohamad@gmail.com>
2021-08-30 13:39:31 +0430

    last commit
    writing an action(post) method with the same name of Get method but with the argument of model
    writing code in action method section

--- TacOcaT-in-MVC/Controllers/HomeController.cs
+++ TacOcaT-in-MVC/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TacOcaT_in_MVC.Models;
 
@@ -27,10 +28,37 @@ namespace TacOcaT_in_MVC.Controllers
         {
             return View();
         }
+
+        [HttpGet]
         public IActionResult ReverseInput()
         {
             TacOcaT model = new();
             return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ReverseInput(TacOcaT tacOcaT)
+        {
+            string inputWord = tacOcaT.InputWord;
+            inputWord = Regex.Replace(inputWord.ToLower(), "[^a-z0-9]+","");
+            string revWord = "";
+
+            for(int i = inputWord.Length - 1; i >= 0; i--)
+            {
+                revWord += inputWord[i];
+            }
+            tacOcaT.RevWord = revWord;
+            if (inputWord == revWord)
+            {
+                tacOcaT.IsTacO = true;
+                tacOcaT.Message = $"Well done! {inputWord} is a palindrom...";
+            }
+            else
+            {
+                tacOcaT.IsTacO = false;
+                tacOcaT.Message = $"Oops! {inputWord} is not a palindrom";
+            }
+            return View(tacOcaT);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
